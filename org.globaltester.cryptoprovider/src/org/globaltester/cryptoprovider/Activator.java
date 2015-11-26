cd72bb3cfb57f3aecd2f68b60f902e60ce2940f7
Alexander May <alexander.may@hjp-consulting.com>
2015-11-26 15:18:22 +0100

    Move accessor class for cryptoprovider service to o.g.cryptoprovider

---
+++ org.globaltester.cryptoprovider/src/org/globaltester/cryptoprovider/Activator.java
@@ -0,0 +1,24 @@
+package org.globaltester.cryptoprovider;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public static BundleContext context;
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		Activator.context = context;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		Activator.context = null;
+	}
+
+	public static BundleContext getContext() {
+		return context;
+	}
+
+}
