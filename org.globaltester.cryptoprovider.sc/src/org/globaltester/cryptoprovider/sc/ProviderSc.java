4c1f2a484d189909782fa50c7aa1e6bffc212b8d
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2016-03-03 12:56:48 +0100

    set service properties at service registration

--- org.globaltester.cryptoprovider.sc/src/org/globaltester/cryptoprovider/sc/ProviderSc.java
+++ org.globaltester.cryptoprovider.sc/src/org/globaltester/cryptoprovider/sc/ProviderSc.java
@@ -2,6 +2,7 @@ package org.globaltester.cryptoprovider.sc;
 
 import java.security.Provider;
 import java.security.Security;
+import java.util.Hashtable;
 
 import org.spongycastle.jce.provider.BouncyCastleProvider;
 import org.globaltester.cryptoprovider.Cryptoprovider;
@@ -14,15 +15,24 @@ public class ProviderSc implements Cryptoprovider {
 		 provider = new BouncyCastleProvider();
 		 Security.addProvider(provider);
 	}
-
+	
 	@Override
 	public String getCryptoProviderString() {
 		return provider.getName(); //returns "SC"
 	}
-
+	
 	@Override
 	public Provider getCryptoProviderObject() {
 		return provider;
 	}
-
+	
+	@Override
+	public Hashtable<String, String> getProperties() {
+		Hashtable<String, String> props = new Hashtable<String, String>();
+		props.put(Cryptoprovider.IDENTIFIER, getCryptoProviderObject().getName());
+		props.put(Cryptoprovider.VERSION, String.valueOf(getCryptoProviderObject().getVersion()));
+		props.put(Cryptoprovider.INFO, getCryptoProviderObject().getInfo());
+		return props;
+	}
+	
 }
